88b7e457b3bb4aa3c6df3ec004cca8760852fc71
Luan01072001 <luanngo01072001@gmail.com>
2021-05-17 21:36:44 +0700

    public

--- resources/js/luanngo.js
+++ resources/js/luanngo.js
@@ -1,34 +1,34 @@
 $(document).ready(
-    function(){
+    function () {
 
         // sticky nav
         $('.about-section').waypoint(
-            function(direction){
-                if(direction == "down"){
+            function (direction) {
+                if (direction == "down") {
                     $('nav').addClass('sticky');
-                }else {
+                } else {
                     $('nav').removeClass('sticky');
                 }
-            },{
-                offset: '500px'
-            }
+            }, {
+            offset: '500px'
+        }
         )
         // Scroll
-        $('a').click(function(event){
+        $('a').click(function (event) {
             $('html, body').animate({
-                scrollTop: $( $.attr(this, 'href') ).offset().top
+                scrollTop: $($.attr(this, 'href')).offset().top
             }, 700);
             event.preventDefault();
         });
         $('.mobile-nav-icon').click(
-            function(){
+            function () {
                 $('.menu').slideToggle(300);
 
-                if($('.mobile-nav-icon').hasClass('fa-bars')){
+                if ($('.mobile-nav-icon').hasClass('fa-bars')) {
                     $('.mobile-nav-icon').addClass('fa-times');
                     $('.mobile-nav-icon').removeClass('fa-bars')
                 }
-                else{
+                else {
                     $('.mobile-nav-icon').addClass('fa-bars');
                     $('.mobile-nav-icon').removeClass('fa-times')
                 }
